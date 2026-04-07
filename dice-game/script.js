cdbe1d38489c459670abd5dac341db410829f887
Sohaima <sohaimanasir1@gmail.com>
2026-04-07 12:37:01 +0500

    2 player dice game

---
+++ dice-game/script.js
@@ -0,0 +1,37 @@
+// *********** 2 PLAYER DICE GAME ************
+
+var rounds = Number(prompt("how many rounds do you want to play"));
+
+var playerOneScore = 0;
+var playerTwoScore = 0;
+
+for (var i = 1; i <= rounds; i++){
+    var playerOneRoll = Math.floor((Math.random()*6) + 1);
+    console.log("player one: " + playerOneRoll);
+    
+    var playerTwoRoll = Math.floor((Math.random()*6) + 1);
+    console.log("player two: " + playerTwoRoll);
+
+    if(playerOneRoll > playerTwoRoll){
+        playerOneScore++;
+    }else if(playerTwoRoll > playerOneRoll){
+        playerTwoScore++;
+    }else{
+        console.log("Draw");
+        
+    }
+
+
+}
+
+console.log("Player 1 Score: " + playerOneScore + " and Player 2 Score: " + playerTwoScore);
+
+
+
+if(playerOneScore > playerTwoScore){
+    alert("Player 1 Wins!!!")
+}else if(playerTwoScore > playerOneScore){
+    alert("Player 2 Wins!!!")
+}else{
+    alert("Same Score. Draw!!")
+}
