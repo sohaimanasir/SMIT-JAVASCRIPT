cf0f64faceb665f63d07defec7c79f852bd38605
Sohaima <sohaimanasir1@gmail.com>
2026-04-11 13:07:52 +0500

    assignment 10-14 init

---
+++ assignment(10-14)/script.js
@@ -0,0 +1,47 @@
+// ************** ASSIGNMENT 10-14 **************
+
+// ======= Question 01 =========
+
+// var userInput = prompt("Enter city name");
+// var cityName = userInput.toLowerCase();
+
+// if(cityName==="karachi"){
+//     alert("Welcome to the city of lights")
+// }
+// else{
+//     alert("Welcome to " + cityName)
+// }
+
+
+
+// ====== Question 02 ==========
+
+// var userInput = prompt("Enter your gender");
+// var userGender = userInput.toLowerCase();
+
+// if(userGender === "female"){
+//     alert("Good Morning, Madam!");
+
+// }else if(userGender === "male"){
+//     alert("Good Morning, Sir!");
+// }else{
+//     alert("Good Morning " + userGender + " !");
+// }
+
+
+
+// ====== Question 03 =========
+
+// var userInput = prompt("Enter color here to check traffic light rule");
+
+// var userColor = userInput.toLowerCase();
+
+// if(userColor === "red"){
+//     alert("Must Stop!!")
+// }else if(userColor === "green"){
+//     alert("Move Now!!")
+// }else if(userColor === "yellow"){
+//     alert("Ready to move!!")
+// }else{
+//     alert ("Pretty color but its not in traffic light")
+// }
