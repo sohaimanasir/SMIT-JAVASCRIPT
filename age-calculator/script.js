7d73bb0d6c09ad61006bcced64f6850ede2020a3
Sohaima <sohaimanasir1@gmail.com>
2026-04-18 00:41:55 +0500

    age calculator

--- age-calculator/script.js
+++ age-calculator/script.js
@@ -1,48 +1,48 @@
-// // =========== CAlCULATING AGE BY DATE OF BIRTH ===========
+// =========== CAlCULATING AGE BY DATE OF BIRTH ===========
 
-// // var dateOfBirth = new Date("May 03, 2002");
-// var dateOfBirth = new Date(prompt("Ener your date of birth 'Jan 01, 2001' format"));
-// var todaysDate = new Date();
+// var dateOfBirth = new Date("May 03, 2002");
+var dateOfBirth = new Date(prompt("Ener your date of birth 'Jan 01, 2001' format"));
+var todaysDate = new Date();
 
-// // => Calculating time from todays date
+// => Calculating time from todays date
 
-// var todaysDateMs = todaysDate.getTime();
-// var dateOfBirthMs = dateOfBirth.getTime();
+var todaysDateMs = todaysDate.getTime();
+var dateOfBirthMs = dateOfBirth.getTime();
 
-// // => Calculating difference in Time
+// => Calculating difference in Time
 
-// var diff = todaysDateMs - dateOfBirthMs;
+var diff = todaysDateMs - dateOfBirthMs;
 
-// // => converting millisecond to yearss 
+// => converting millisecond to yearss 
 
-// var oneSecond = 1000;
+var oneSecond = 1000;
 
-// var oneMin = oneSecond * 60;
+var oneMin = oneSecond * 60;
 
-// var oneHour = oneMin * 60;
+var oneHour = oneMin * 60;
 
-// var oneDay = oneHour * 24;
+var oneDay = oneHour * 24;
 
-// var oneMonth = oneDay * 30;
+var oneMonth = oneDay * 30;
 
-// var oneYear = oneDay * 365.25;
+var oneYear = oneDay * 365.25;
 
-// // => Now for difference
+// => Now for difference
 
-// var totalYears = Math.floor(diff / oneYear);
-// console.log(totalYears);
+var totalYears = Math.floor(diff / oneYear);
+console.log(totalYears);
 
-// var remainingTimeAfterYears = diff % oneYear;
-// var totalMonths = Math.floor(remainingTimeAfterYears / oneMonth);
-// console.log(totalMonths);
+var remainingTimeAfterYears = diff % oneYear;
+var totalMonths = Math.floor(remainingTimeAfterYears / oneMonth);
+console.log(totalMonths);
 
-// var remainingTimeAfterMonths = remainingTimeAfterYears % oneMonth;
-// var totalDays = Math.floor(remainingTimeAfterMonths / oneDay)
-// console.log(totalDays);
+var remainingTimeAfterMonths = remainingTimeAfterYears % oneMonth;
+var totalDays = Math.floor(remainingTimeAfterMonths / oneDay)
+console.log(totalDays);
 
-// // => Final Result
+// => Final Result
 
-// alert("You are " + totalYears + " years " + totalMonths + " months " + totalDays + " days old")
+alert("You are " + totalYears + " years " + totalMonths + " months " + totalDays + " days old")
 
 
 
