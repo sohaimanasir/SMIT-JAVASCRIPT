210201f60aebc17ac014af48595239fc56e3a438
Sohaima <sohaimanasir1@gmail.com>
2026-04-17 15:38:08 +0500

    question 1-10 done

---
+++ Assignment-(21-25)/script.js
@@ -0,0 +1,84 @@
+// ******************** CHAPTER 21-25 *******************
+
+// ======= Question 01 =======
+
+// var userInput1 = prompt("write your first name");
+// var userInput2 = prompt("Write your last name");
+
+// var userFullName = userInput1 + userInput2;
+// alert("Welcome "+ userFullName);
+
+
+// // ======= Question 02 =======
+
+// var userMobile = prompt("Enter your favorite mobile phone model");
+// var userInputLength = userMobile.length;
+
+// alert("My favorite phone is: "+ userMobile + "\n Length of String is " + userInputLength);
+
+
+// ======= Question 03 =======
+
+// var userNationality = "Pakistani";
+// var indexOfLetter = userNationality.indexOf('n');
+
+// alert('String: ' + userNationality + ' Index of n is: ' + indexOfLetter)
+
+
+// ======= Question 04 ======
+
+// var stringName = "Hello World!";
+// var lastIndexOfLetter = stringName.lastIndexOf("l");
+
+// alert ("String: " + stringName + "\nLast index of 'l' " + lastIndexOfLetter);
+
+
+// ====== Question 05 ========
+
+// var stringOne = "Pakistani";
+// var charOfLetter =  stringOne.charAt(3);
+
+// console.log("String: " + stringOne + "\nCharacter at index 3 is: " + charOfLetter);
+
+
+// ======= Question 06 ======
+
+// var firstName =  prompt("Enter first Name");
+// var lastName = prompt("Enter last name");
+
+// var  fullName = firstName.concat(" ", lastName);
+
+// console.log("Good Morning " + fullName);
+
+
+
+// ======= Question 07 ======
+
+// var cityName = "Hyderabad";
+// var newCityName = cityName.replace("Hyder" , "Islam");
+
+// console.log("City Name is: " + cityName + "\nNew city name is: " + newCityName);
+
+
+// ======= Question 08 ======
+
+// var message = "Ali and Sara are best friends. They play cricket and football together";
+// var newMessage = message.replaceAll("and" , "&");
+
+// console.log(newMessage);
+
+
+// ====== Question 09 ======
+
+// var newString = "472";
+// var stringToNumber = parseInt(newString);
+
+// console.log("Value: " + newString + "\nType: " + typeof(newString) + "\nValue: " + stringToNumber + "\nType: " + typeof(stringToNumber));
+
+
+// ====== Question 10 =======
+
+// var userInput = prompt("Enter a word to make it uppercase");
+// var newUserInput = userInput.toUpperCase();
+
+// console.log(newUserInput);
