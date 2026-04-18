44126969517e4dbeee9448deb730a5fb6e933eec
Sohaima <sohaimanasir1@gmail.com>
2026-04-18 11:54:57 +0500

    assignment(15-17) q(1-6) done

---
+++ Assignment(15-17)/script.js
@@ -0,0 +1,40 @@
+// *********** CHAPTER 15-17 *************
+
+// ======= Question 01 ========
+
+// JS literal notation
+
+var studentNames = [];
+console.log(studentNames);
+
+// ======= Question 02 ========
+
+// JS object notation
+
+var studentsObject = new Array();
+console.log(studentsObject);
+
+// ======= Question 03 ========
+
+var strArray = ['apple' , 'banana' , 'peach' , 'grapes'];
+console.log(strArray);
+
+// ======= Question 04 ========
+
+var numArray = [23, 54, 1, 99, 0];
+console.log(numArray);
+
+// ======= Question 05 ========
+
+var booleanArr = [true, false, true, true, false]
+console.log(booleanArr);
+
+// ======= Question 06 =======
+
+var mixArray = [23, "Sohaima", true, '']
+console.log(mixArray);
+
+
+
+
+
