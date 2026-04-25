be5aec1205f9180c56d5ab639912e27505799c06
Sohaima <sohaimanasir1@gmail.com>
2026-04-25 12:37:30 +0500

    calculation function

--- Functions/script.js
+++ Functions/script.js
@@ -5,3 +5,38 @@ function greetings(){
 }
 
 // greetings();
+
+// ===== function for reversing string =======
+
+function reverseStr(nameReverse){
+    for(i = nameReverse.length-1; i >= 0; i--){
+    console.log(nameReverse[i]);
+    }
+}
+
+// var userName = prompt("Enter your name")
+reverseStr(userName);   //inside paranthesis its argument and in function's paranthesis its parameter
+
+// Function of Calculations
+
+function calc(num1 , num2, operator){
+    if (operator=== "+"){
+        console.log(num1 + num2);
+        
+    }else if(operator === '-'){
+        console.log(num1 - num2);
+        
+    }else if(operator === '*'){
+        console.log(num1 * num2);
+        
+    }else if(operator === '/'){
+        console.log(num1 / num2);
+        
+    }
+}
+
+var userNum1 = parseInt(prompt("Enter 1st number"));
+var userNum2 = parseInt(prompt("Enter 2nd number"));
+var userOperator = prompt("Enter any operator +,-,*,/")
+
+calc(userNum1, userNum2, userOperator);
