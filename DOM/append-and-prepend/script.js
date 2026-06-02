29e797d63a53197d270cd8e16e2ff06ff97c1814
Sohaima <sohaimanasir1@gmail.com>
2026-06-02 12:50:23 +0500

    append and prepend

---
+++ DOM/append-and-prepend/script.js
@@ -0,0 +1,47 @@
+// var heading2 = document.createElement("h2");
+// heading2.textContent = "hello";
+// heading2.id = "head-1";
+
+
+// heading2.classList.add("class-1", "class-2");
+// heading2.style.backgroundColor = "lightblue";
+// heading2.style.textAlign = "center";
+// console.dir(heading2);
+
+// var heading1 = document.getElementById ("head-1");
+// heading1.style.backgroundColor = "pink";
+// heading1.after (heading2);
+
+
+// ======== append and prepend
+
+// var list1 = document.createElement("li")
+// list1.textContent = "Milk";
+// var list2 = document.createElement("li")
+// list2.textContent = "Butter";
+// var list3 = document.createElement("li")
+// list3.textContent = "Yogurt";
+
+// var list = document.getElementById("unorder");
+
+// // list.append(list1);
+// // list.prepend(list3);
+// // list.append(list2);
+
+
+
+// console.log(list);
+
+// var items = ["milk" , "butter" , "cream"];
+// for (var i = 0 ; i < items.length ; i++){
+//     var list = document.createElement("li");
+//     list.textContent = items[i]
+//     unorder.append(list)
+    
+// }
+
+// ====== delete =====
+
+var removeHead = document.getElementById("head-1");
+removeHead.remove();
+
