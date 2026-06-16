b821d874892a596866668f2f674fa4fe86239985
Sohaima <sohaimanasir1@gmail.com>
2026-06-16 11:37:50 +0500

    events

---
+++ Events/script.js
@@ -0,0 +1,30 @@
+// ========= EVENTS ==========
+
+function popMsg(username){
+    // var userName = "Sohaima";
+    alert("Hello " + username)
+}
+function changeColor(anchortag){
+
+    console.dir(anchortag.style.color = 'green');
+    console.log(anchortag.style.color = 'green');
+    
+    
+}
+
+function changeText(heading1){
+    console.dir(heading1.textContent = 'Bye')
+}
+
+function reverseText(heading1){
+    console.dir(heading1.textContent = 'Hello World')
+}
+
+function expand(){
+    document.getElementById(para.style.display = 'block');
+
+}
+
+function showLess(){
+    document.getElementById(para.style.display = 'none')
+}
